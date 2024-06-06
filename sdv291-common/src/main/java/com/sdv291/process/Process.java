fdf3566043f91a550b3ff7b8874c2eac5ef1b5c7
sdv291 <86102700+sdv291@users.noreply.github.com>
2024-06-06 23:11:48 +0300

    Update Process.java

--- sdv291-common/src/main/java/com/sdv291/process/Process.java
+++ sdv291-common/src/main/java/com/sdv291/process/Process.java
@@ -1,13 +1,47 @@
 package com.sdv291.process;
 
+/**
+ * Process interface for easy operation.
+ *
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #offer(Object, long)}.
+ *
+ * @param <T> the type of the input to the operation
+ *
+ * @author Dmitry Salanzhyi
+ */
 @FunctionalInterface
 public interface Process<T> {
 
   long UNLIMITED_TIMEOUT = 0L;
 
-  default boolean offer(T holder) throws InterruptedException {
-    return offer(holder, UNLIMITED_TIMEOUT);
+  /**
+   * Add the specified data into the process.
+   *
+   * @param data the element to add for async handle
+   *
+   * @return {@code true} if the element was added to this process,
+   * otherwise {@code false}
+   *
+   * @throws InterruptedException if interrupted while waiting
+   * @throws NullPointerException if the specified element is null
+   */
+  default boolean offer(T data) throws InterruptedException {
+    return offer(data, UNLIMITED_TIMEOUT);
   }
 
-  boolean offer(T holder, long timeout) throws InterruptedException;
+  /**
+   * Add the specified data into the process, waiting up to the
+   * specified wait time if necessary for space to become available.
+   *
+   * @param data    the element to add for async handle
+   * @param timeout timeout in millis
+   *
+   * @return {@code true} if the element was added to this process,
+   * otherwise {@code false}
+   *
+   * @throws InterruptedException if interrupted while waiting
+   * @throws NullPointerException if the specified element is null
+   */
+  boolean offer(T data, long timeout) throws InterruptedException;
 }
